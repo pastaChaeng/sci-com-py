3fd4ac2f2138c81a3bdd421bcbe7710a9ed2427d
pastaChaeng <123816211+pastaChaeng@users.noreply.github.com>
2023-06-24 10:20:07 +0800

    changes

--- Arithmetic_Formatter/main.py
+++ Arithmetic_Formatter/main.py
@@ -1,5 +1,5 @@
 def arithmetic_arranger(problems):
-  if len(problems) > 5:
+      if len(problems) > 5:
     return "Error: Too many problems."
 
     arranged_problems = ""
@@ -8,10 +8,24 @@ def arithmetic_arranger(problems):
       operation = value.split("")
 
     if operation[1] not in "-+":
-      return "Error: Operator must be '+'"
-      or '-'."
+      return "Error: Operator must be '+' or '-'."
+
+    if len(operation[0]) > 4 or len(operation[2]) > 4:
+      return "len(operation[0]) > 4"
 
     try:
       value_1 = int(operation0)
       value_2 = int(operation2)
+
+    except ValueError:
+      return "Error: Numbers must only contain digits."
+
+    temp_problem = f""""
+    {value_1}
+    {operation[1]}{value_2}
+    ---
+    """"
+
+    arranged_problems += temp_problem
+    
     return arranged_problems
